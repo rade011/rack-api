9923106a0f2bc2054d5586038c2ca9274168eff6
Rade <rade.radivojevic@gmail.com>
2019-05-21 11:06:41 +0200

    Prepare app for heroku deploy

--- src/config/dev.js
+++ src/config/dev.js
@@ -2,5 +2,8 @@ export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl: 'mongodb://localhost:27017/rack-api'
+
+  dbUrl:
+    'mongodb://rade:r23571113@cluster0-shard-00-00-alcaf.mongodb.net:27017,cluster0-shard-00-01-alcaf.mongodb.net:27017,cluster0-shard-00-02-alcaf.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin'
 }
+// dbUrl: 'mongodb://localhost:27017/rack-api'
