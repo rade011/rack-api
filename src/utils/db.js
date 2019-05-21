d636723a3f3c39063103a2dea080814f222ffb7e
Rade <rade.radivojevic@gmail.com>
2019-05-21 15:44:22 +0200

    update db url

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,5 +2,15 @@ import mongoose from 'mongoose'
 import options from '../config'
 
 export const connect = (url = options.dbUrl, opts = {}) => {
-  return mongoose.connect(url, { ...opts, useNewUrlParser: true })
+  return mongoose.connect(
+    url,
+    { ...opts, useNewUrlParser: true },
+    (err, res) => {
+      if (err) {
+        console.log('DB Connetct error :' + url + '. ' + err)
+      } else {
+        console.log('Succeeded connected to: ' + url)
+      }
+    }
+  )
 }
