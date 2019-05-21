f9e3604122186368e497dd8fcff3faaa292a0b14
Rade <rade.radivojevic@gmail.com>
2019-05-21 15:59:08 +0200

    update db url

--- src/config/dev.js
+++ src/config/dev.js
@@ -2,8 +2,5 @@ export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl:
-    process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
-    'mongodb://localhost:27017/rack-api'
+  dbUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/rack-api'
 }
